a66730a8a650866f6fcafeb3f6819a74aa9d181b
bredele <olivier.wietrich@gmail.com>
2016-10-20 11:27:55 -0600

    pass default parameters

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 
 var update = require('pheromone')
 var exploitation = require('exploitation')
-var distance = require('euclidean')
+var eulidean = require('euclidean')
 
 
 /**
@@ -17,10 +17,10 @@ var distance = require('euclidean')
  * @api public
  */
 
-module.exports = function(ants, τ = 1, ρ = 0.1, α = 0.1) {
+module.exports = function(ants, β = 2, τ = 1, ρ = 0.1, α = 0.1, distance = euclidean) {
   var pheromones = []
   return function(cb) {
-    var {best, length} = step(ants, pheromones, τ, ρ, cb)
+    var {best, length} = step(ants, pheromones, τ, ρ, β, distance, cb)
     // global update
     best.reduce((r, s) => {
       update(pheromones[r][s], 1 / length, α)
@@ -39,12 +39,14 @@ module.exports = function(ants, τ = 1, ρ = 0.1, α = 0.1) {
  * @param {Array} pheromones
  * @param {Number} delta
  * @param {Number} ρ (local decay)
+ * @param {Number} β
+ * @param {Function} distance
  * @param {Function} cb (optional)
  * @return {Object}
  * @api private
  */
 
-function step(ants, pheromones, delta, ρ, cb) {
+function step(ants, pheromones, delta, ρ, β, distance, cb) {
   var best = []
   var length = 0
   var j = ants.length - 1
@@ -52,7 +54,7 @@ function step(ants, pheromones, delta, ρ, cb) {
     var visited = []
     visited.push(r)
     var tour = 0
-    var transition = exploitation(r, ants)
+    var transition = exploitation(r, ants, β, distance)
     while(j--) {
       let s = transition(pheromones, cb)
       visited.push(s)
